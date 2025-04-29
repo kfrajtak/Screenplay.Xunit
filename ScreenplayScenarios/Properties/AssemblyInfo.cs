c1fb0ffed0e4548748a13deb5c4214b09a32c981
Karel Frajták <1510452+kfrajtak@users.noreply.github.com>
2025-04-29 07:35:46 +0200

    upgraded to new csproj

--- ScreenplayScenarios/Properties/AssemblyInfo.cs
+++ ScreenplayScenarios/Properties/AssemblyInfo.cs
@@ -1,22 +1,9 @@
-using ScreenplayScenarios;
+﻿using ScreenplayScenarios;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ScreenplayScenarios")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScreenplayScenarios")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("88bd1942-538c-41dd-bbff-5ccbe020581d")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 //[assembly: Screenplay.XUnit.ScreenplayAssembly(typeof(ScreenplayIntegrationConfig))]
